cf5f895e0995af884d82715b0a2a3ff4b27e996b
AlexCrocker <alex.b.crocker@gmail.com>
2013-11-20 18:20:04 -0800

    alex commit pre merge with mark

---
+++ SCADA911Project/SCADA911Project/UserList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SCADA911Project
+{
+
+    enum ContactMethodTypes { phone, sms, email };
+
+    struct ContactMethod
+    {
+        private string ContactMethodName;
+        private ContactMethodTypes ContactMethodType; 
+        private string ContactString;
+    }
+
+    struct UserListEntry
+    {
+        private string UserName;
+
+        
+    }
+
+    class UserList
+    {
+
+    }
+}
